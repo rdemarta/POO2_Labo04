b4ae2fc6a429ed8c09f614d916dd82f04f9ad05f
Robin <r.demarta@outlook.com>
2020-04-30 16:44:31 +0200

    Base project

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/**
+ * File: main.cpp
+ * Authors: Robin Demarta & Loïc Dessaules
+ * Date: 30.04.2020
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
